9fcddfdfd9c19f655636cebfe8399d846479f9bb
Victor <horoyami@yandex.ru>
2021-01-21 04:08:35 -0800

    WOW! I can make a move

--- GameCycle.hs
+++ GameCycle.hs
@@ -11,17 +11,28 @@ gameCycle = do
   makeMove makeEmptyBoard 1
 
 makeMove :: [[Cell]] -> Int -> IO ()
-makeMove broad player = do
+makeMove board player = do
   putStrLn ("Player" ++ (show player) ++" makes a move (a1-h8):")
   input <- getLine
   let coords = parseNote input
   if coords /= Nothing then do
       let (Just (x1, y1, x2, y2)) = coords
       let figureColor = if player == 1 then whiteFigure else blackFigure
-      newBoard <- move broad 1 figureColor x1 y2 x2 y2
-      makeMove broad player
+      newBoard <- move board 1 figureColor x1 y1 x2 y2
+      let isBadMove = newBoard == board
+      let text = if isBadMove then "Bad move!" else ("Player" ++ (show player) ++" made a move " ++ input)
+      reshowScreen newBoard text
+      makeMove board (player `mod` 2 + (if isBadMove then 0 else 1))
     else do
       cursorUp 3
       clearFromCursorToScreenEnd
-      putStrLn "Bad input"
-      makeMove broad player
+      putStrLn "Bad input!"
+      makeMove board player
+
+reshowScreen :: [[Cell]] -> String -> IO ()
+reshowScreen board text = do
+  clearScreen
+  setCursorPosition 0 0
+  showBoard board
+  putStr "\n"
+  putStrLn text
