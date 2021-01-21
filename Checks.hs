df984c5a3343adb931e02344e0c6cb3945172416
Victor <horoyami@yandex.ru>
2021-01-21 05:19:50 -0800

    2 step move for pawn

--- Checks.hs
+++ Checks.hs
@@ -16,18 +16,31 @@ parseNote (s1 : s2 : '-' : s3 : s4 : []) | check s1 s2 s3 s4 = Just (letterToCoo
       checkD c = c >= '1' && c <= '8'
 parseNote _ = Nothing
 
---------
-
 move :: [[Cell]] -> Int -> Word8 -> Int -> Int -> Int -> Int -> IO [[Cell]]
-move board step playerColor x1 y1 x2 y2 | getFigure board x1 y1 == pawn playerColor = return (movePawn board step x1 y1 x2 y2)
+move board step playerColor x1 y1 x2 y2 | getFigure board x1 y1 == pawn playerColor = return (movePawn board playerColor step x1 y1 x2 y2)
                                         | otherwise = return board
 
-movePawn board step x1 y1 x2 y2 = do
+movePawn :: [[Cell]] -> Word8 -> Int -> Int -> Int -> Int -> Int -> [[Cell]]
+movePawn board playerColor step x1 y1 x2 y2 = do
   let x = x2 - x1
   let y = y2 - y1
 
-  case (x, y) of
-    (0, 1) -> moveWhitePawnOneStep board x1 y1
+  if playerColor == whiteFigure && (x, y) == (0, 1) then moveWhitePawnOneStep board x1 y1
+  else if playerColor == blackFigure && (x, y) == (0, -1) then moveBlackPawnOneStep board x1 y1
+  else if playerColor == whiteFigure && (x, y) == (0, 2) && y1 == 2 then moveWhitePawnTwoStep board x1 y1
+  else if playerColor == blackFigure && (x, y) == (0, -2) && y1 == 7 then moveBlackPawnTwoStep board x1 y1
+  else board
+
+moveWhitePawnTwoStep :: [[Cell]] -> Int -> Int -> [[Cell]]
+moveWhitePawnTwoStep board x1 y1 = if getFigure board x1 (y1+1) /= Empty || getFigure board x1 (y1+2) /= Empty
+                                     then board else changeBoard board x1 y1 x1 (y1+2)
+
+moveBlackPawnTwoStep :: [[Cell]] -> Int -> Int -> [[Cell]]
+moveBlackPawnTwoStep board x1 y1 = if getFigure board x1 (y1-1) /= Empty || getFigure board x1 (y1-2) /= Empty
+                                     then board else changeBoard board x1 y1 x1 (y1-2)
 
 moveWhitePawnOneStep :: [[Cell]] -> Int -> Int -> [[Cell]]
 moveWhitePawnOneStep board x1 y1 = if getFigure board x1 (y1+1) /= Empty then board else changeBoard board x1 y1 x1 (y1+1)
+
+moveBlackPawnOneStep :: [[Cell]] -> Int -> Int -> [[Cell]]
+moveBlackPawnOneStep board x1 y1 = if getFigure board x1 (y1-1) /= Empty then board else changeBoard board x1 y1 x1 (y1-1)
--- GameCycle.hs
+++ GameCycle.hs
@@ -8,26 +8,27 @@ import Figures
 
 gameCycle :: IO ()
 gameCycle = do
-  makeMove makeEmptyBoard 1
+  makeMove makeEmptyBoard 1 1
 
-makeMove :: [[Cell]] -> Int -> IO ()
-makeMove board player = do
+makeMove :: [[Cell]] -> Int -> Int -> IO ()
+makeMove board player step = do
   putStrLn ("Player" ++ (show player) ++" makes a move (a1-h8):")
   input <- getLine
   let coords = parseNote input
   if coords /= Nothing then do
       let (Just (x1, y1, x2, y2)) = coords
       let figureColor = if player == 1 then whiteFigure else blackFigure
-      newBoard <- move board 1 figureColor x1 y1 x2 y2
+      newBoard <- move board step figureColor x1 y1 x2 y2
       let isBadMove = newBoard == board
       let text = if isBadMove then "Bad move!" else ("Player" ++ (show player) ++" made a move " ++ input)
       reshowScreen newBoard text
-      makeMove board (player `mod` 2 + (if isBadMove then 0 else 1))
-    else do
+      let nextPlayer = player `mod` 2 + 1
+      makeMove newBoard (if isBadMove then player else nextPlayer) (if isBadMove then step else (step + 1))
+  else do
       cursorUp 3
       clearFromCursorToScreenEnd
       putStrLn "Bad input!"
-      makeMove board player
+      makeMove board player (step + 1)
 
 reshowScreen :: [[Cell]] -> String -> IO ()
 reshowScreen board text = do
